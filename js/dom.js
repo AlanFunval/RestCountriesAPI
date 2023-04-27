7d82dea10077cc7673b9171fa8aa721062946665
Alan Cano <prueba@correo.com>
2023-04-26 23:23:09 -0600

    cambios

--- js/dom.js
+++ js/dom.js
@@ -78,15 +78,6 @@ const showCardDetails = (obj) => {
   return div;
 };
 
-const cards = [...countries.children];
-cards.forEach((card) => {
-  card.addEventListener('click', () => {
-    const cardDetails = dom.showCardDetails(countriesAPI.find((obj) => obj.name.common === card.querySelector('h5').textContent));
-    countries.innerHTML = '';
-    countries.appendChild(cardDetails);
-  });
-});
-
 export default {
     Card,
     $,
